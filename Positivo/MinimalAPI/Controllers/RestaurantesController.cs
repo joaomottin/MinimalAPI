56629288bbeb6c787a0b8286b05674d231a4bc30
ADEMIR JOSE WILSEK SCARPIM <ademir.jose@cs.up.edu.br>
2025-04-28 19:40:54 -0300

    Correção do código no RestaurantesController

--- Positivo/MinimalAPI/Controllers/RestaurantesController.cs
+++ Positivo/MinimalAPI/Controllers/RestaurantesController.cs
@@ -1,79 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
 using API.Modelos;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Namespace;
 
-namespace Namespace;
+namespace API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class RestaurantesController : ControllerBase
+[Route("api/restaurantes")]
+public class RestauranteController : ControllerBase
 {
     private readonly AppDataContext _context;
-
-    public RestaurantesController(AppDataContext context)
-    {
-        _context = context;
+    public RestauranteController(AppDataContext context) {
+        this._context = context;
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Restaurante>>> Get()
-    {
-        return await _context.Restaurantes.ToListAsync();
+    public IActionResult ListAll() {
+        List<Restaurante> restaurantes = _context.Restaurantes.ToList();
+        return Ok(restaurantes);
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Restaurante>> Get(int id)
-    {
-        var restaurante = await _context.Restaurantes.FindAsync(id);
-
-        if (restaurante == null)
+    public IActionResult FindById(int id) {
+        var restaurante = _context.Restaurantes.Find(id);
+        if (restaurante == null) {
             return NotFound();
-
-        return restaurante;
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<Restaurante>> Post(Restaurante restaurante)
-    {
-        _context.Restaurantes.Add(restaurante);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction(nameof(Get), new { id = restaurante.Id }, restaurante);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, Restaurante restaurante)
-    {
-        if (id != restaurante.Id)
-            return BadRequest();
-
-        _context.Entry(restaurante).State = EntityState.Modified;
-
-        try
-        {
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!_context.Restaurantes.Any(r => r.Id == id))
-                return NotFound();
-            else
-                throw;
         }
 
-        return NoContent();
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(int id)
-    {
-        var restaurante = await _context.Restaurantes.FindAsync(id);
-        if (restaurante == null)
-            return NotFound();
-
-        _context.Restaurantes.Remove(restaurante);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
+        return Ok(restaurante);    
     }
 }
