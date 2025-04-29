40acd7dd895071d10b4d803245c400258068e44b
João Mottin <joaopedromm.construtiva@outlook.com>
2025-04-28 23:26:07 -0300

    realizando testes e corrigindo bugs

--- Positivo/MinimalAPI/Program.cs
+++ Positivo/MinimalAPI/Program.cs
@@ -1,24 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Namespace;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDataContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
